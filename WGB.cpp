dbf5243f6a6f7b03eea2b5c5451894cc5c5c4424
olsonso <olson1sonja@gmail.com>
2018-03-06 15:51:50 -0800

    Update WGB.cpp

--- WGB.cpp
+++ WGB.cpp
@@ -12,6 +12,7 @@
 
 #include <iostream>
 #include <string>
+//#include "WGB.h"
 #include "BankAccount.h"
 
 using namespace std;
@@ -27,6 +28,9 @@ int main(){
 	string withdraw_amount;
 
 	BankAccount obj;
+	std::cout << "\n ___________________________________________\n" << endl;
+	std::cout << "\nWelcome to the Worlds Greatest Bank Ledger!\n" << endl;
+	std::cout << "\n ___________________________________________\n" << endl;
 	obj.menu();
 
 	return 0;
