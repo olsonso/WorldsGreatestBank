0cccf43939ee9799de2a47a13d4be6219f7e35ed
olsonso <olson1sonja@gmail.com>
2018-03-06 11:55:09 -0800

    Attempting .NET with C#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorldsGreatestBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Worlds Greatest Bank Ledger");
+        }
+    }
+}
