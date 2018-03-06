948f47897522fcbd5317f939f9762acb88640e1f
olsonso <olson1sonja@gmail.com>
2018-03-06 11:19:15 -0800

    Update BankAccount.h

--- BankAccount.h
+++ BankAccount.h
@@ -1,6 +1,8 @@
 #ifndef BANKACCOUNT_H
 #define BANKACCOUNT_H
 
+
+
 using namespace std;
 
 class BankAccount
@@ -8,16 +10,21 @@ class BankAccount
 public:
 	char username;
 	char password;
+	string accountName;
 
 	BankAccount();
+
+	//Constructor
 	BankAccount(double inital_amount, std::string name);
 
 	void menu(void);
+	void createNewAccount();
 	void deposit(double amount);
 	void withdraw(double amount);
-	void balance(double amount);
-	void transactionHistory();
-  
+	void checkAccountBalance(string accountName);
+	// void transactionHistory(void);
+	
+	//Acessors
 	double amount() const;
 	double inital_amount;
 
