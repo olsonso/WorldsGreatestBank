0bc2175f4e35ee05707a10ca8e32fd33d818fa5a
olsonso <olson1sonja@gmail.com>
2018-03-07 11:43:20 -0800

    Update WGB.cpp

--- WGB.cpp
+++ WGB.cpp
@@ -1,15 +1,3 @@
-/* Program that does the following
-
--Create a new account
--Login
--Record a deposit
--Record a withdrawal
--Check balance
--See transaction history
--Log out
-
-*/
-
 #include <iostream>
 #include <string>
 #include "BankAccount.h"
